ca05afd7a6e3a2dc4977e37384c935b34fa7b5e0
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-14 09:16:37 -0500

    Initial commit
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/main/java/com/fmontalvoo/springboot/api/SpringbootBackendRestApplication.java
@@ -0,0 +1,13 @@
+package com.fmontalvoo.springboot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendRestApplication.class, args);
+	}
+
+}
